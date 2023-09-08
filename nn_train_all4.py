9f016be633c3e8117130b4eb1b819c517037354b
cainsliewastaken <cainslie@ucsc.edu>
2023-09-08 14:12:20 -0700

    Removed hdf5 again

--- nn_train_all4.py
+++ nn_train_all4.py
@@ -10,7 +10,7 @@ import sys
 #import netCDF4 as nc
 #from prettytable import PrettyTable
 #from count_trainable_params import count_parameters    
-import hdf5storage
+#import hdf5storage
 import pickle
 
 
