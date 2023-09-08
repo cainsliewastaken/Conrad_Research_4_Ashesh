f8b1471dfab77eaa7621a9cc75ec3082ab20fb00
cainsliewastaken <cainslie@ucsc.edu>
2023-09-08 14:29:57 -0700

    FIxed dependency issues

--- eval_networks_plot_rlts.py
+++ eval_networks_plot_rlts.py
@@ -13,7 +13,6 @@ import sys
 #from count_trainable_params import count_parameters    
 #import hdf5storage
 import pickle
-from nn_train_all4 import Net, directstep, Eulerstep, RK4step, PECstep
 import matplotlib.pyplot as plt
 
 
@@ -30,11 +29,81 @@ lead=1
 time_step = 1e-3
 trainN=150000
 input_size = 1024
+hidden_layer_size = 2000
 output_size = 1024
 
 input_test_torch = torch.from_numpy(np.transpose(data[:,trainN:])).float().cuda()
 label_test_torch = torch.from_numpy(np.transpose(data[:,trainN+lead:])).float().cuda()
 label_test = np.transpose(data[:,trainN+lead:])
+
+
+def RK4step(net,input_batch):
+ output_1 = net(input_batch.cuda())
+ output_2 = net(input_batch.cuda()+0.5*output_1)
+ output_3 = net(input_batch.cuda()+0.5*output_2)
+ output_4 = net(input_batch.cuda()+output_3)
+
+ return input_batch.cuda() + time_step*(output_1+2*output_2+2*output_3+output_4)/6
+
+
+def Eulerstep(net,input_batch):
+ output_1 = net(input_batch.cuda())
+ return input_batch.cuda() + time_step*(output_1) 
+  
+
+def directstep(net,input_batch):
+  output_1 = net(input_batch.cuda())
+  return output_1
+
+def PECstep(net,input_batch):
+ output_1 = net(input_batch.cuda()) + input_batch.cuda()
+ return input_batch.cuda() + time_step*0.5*(net(input_batch.cuda())+net(output_1))
+
+def PEC4step(net,input_batch):
+ output_1 = time_step*net(input_batch.cuda()) + input_batch.cuda()
+ output_2 = input_batch.cuda() + time_step*0.5*(net(input_batch.cuda())+net(output_1))
+ output_3 = input_batch.cuda() + time_step*0.5*(net(input_batch.cuda())+net(output_2))
+ return input_batch.cuda() + time_step*0.5*(net(input_batch.cuda())+net(output_3))
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.il  = ((nn.Linear(input_size,hidden_layer_size)))
+        torch.nn.init.xavier_uniform_(self.il.weight)
+
+        self.hidden1  = ((nn.Linear(hidden_layer_size,hidden_layer_size)))
+        torch.nn.init.xavier_uniform_(self.hidden1.weight)
+
+        self.hidden2  = (nn.Linear(hidden_layer_size,hidden_layer_size))
+        torch.nn.init.xavier_uniform_(self.hidden2.weight)
+
+        self.hidden3  = (nn.Linear(hidden_layer_size,hidden_layer_size))
+        torch.nn.init.xavier_uniform_(self.hidden3.weight)
+
+        self.hidden4  = (nn.Linear(hidden_layer_size,hidden_layer_size))
+        torch.nn.init.xavier_uniform_(self.hidden4.weight)
+
+        self.hidden5  = (nn.Linear(hidden_layer_size,hidden_layer_size))
+        torch.nn.init.xavier_uniform_(self.hidden5.weight)        
+
+        self.ol  = nn.Linear(hidden_layer_size,output_size)
+        torch.nn.init.xavier_uniform_(self.ol.weight)
+
+        self.tanh = nn.Tanh()
+
+
+    def forward(self,x):
+        
+        x1 = self.tanh(self.il(x))
+        x2 = self.tanh(self.hidden1(x1))
+        x3 = self.tanh(self.hidden2(x2))
+        x4 = self.tanh(self.hidden3(x3))
+        x5 = self.tanh(self.hidden4(x4))
+        x6 = self.tanh(self.hidden5(x5))
+        out =self.ol(x6)
+        return out
+
+
 
 
 
@@ -49,9 +118,6 @@ mynet_RK4step.load_state_dict(torch.load('NN_RK4step_lead1.pt'))
 
 mynet_PECstep = Net()
 mynet_RK4step.load_state_dict(torch.load('NN_PECstep_lead1.pt'))
-
-
-
 
 
 
