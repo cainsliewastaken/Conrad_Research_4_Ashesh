262536e382a47f4cb1ea9199e4fd28e0ac771cf4
cainsliewastaken <cainslie@ucsc.edu>
2023-09-26 20:14:32 -0700

    fix file path again

--- nn_train_FNO_all3.py
+++ nn_train_FNO_all3.py
@@ -17,7 +17,7 @@ import pickle
 path_outputs = '/media/volume/sdc/conrad_stability/model_eval/'
 
 
-with open('/media/volume/sdc/conrad_temp/training_data/KS_1024.pkl', 'rb') as f:
+with open('/media/volume/sdc/conrad_stability/training_data/KS_1024.pkl', 'rb') as f:
     data = pickle.load(f)
 data=np.asarray(data[:,:250000])
 
