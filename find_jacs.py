920ffc0e4c16a794ce8ca7d4f51b863d46a3ef9e
cainsliewastaken <cainslie@ucsc.edu>
2023-10-04 12:21:30 -0700

    Made matfile

--- find_jacs.py
+++ find_jacs.py
@@ -25,7 +25,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 
 print('loading data')
-path_outputs = '/media/volume/sdb/conrad_stability/model_eval_FNO_tendency/'
+path_outputs = '/media/volume/sdb/conrad_stability/model_eval_tendency/'
 
 with open('/media/volume/sdb/conrad_stability/training_data/KS_1024.pkl', 'rb') as f:
     data = pickle.load(f)
@@ -105,7 +105,7 @@ def PECstep(input_batch):
 
 # mynet = MLP_Net(input_size, hidden_layer_size, output_size)
 mynet = FNO1d(modes, width, time_future, time_history)
-mynet.load_state_dict(torch.load("/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Spectral_Loss_FNO_PECstep_tendency_lambda_reg5lead1.pt"))
+mynet.load_state_dict(torch.load("/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Spectral_Loss_with_tendencyfft_lambda_reg5_directstep_lead1.pt"))
 mynet.eval()
 mynet.cuda()
 
@@ -127,6 +127,6 @@ print(ygrad.shape)
 
 matfiledata = {}
 matfiledata[u'Jacobian_mats'] = ygrad
-scipy.io.savemat(path_outputs+'FNO_KS_PECstep_tendency_lead'+str(lead)+'.mat', matfiledata)
+scipy.io.savemat(path_outputs+'MLP_KS_Directstep_tendency_lead'+str(lead)+'.mat', matfiledata)
 
 print('Saved Predictions')
---
+++ find_plot_eigs.m
@@ -0,0 +1,55 @@
+direct_step_jacs = load('MLP_KS_Directstep_tendency_lead1.mat');
+Euler_step_jacs = load('MLP_KS_Eulerstep_tendency_lead1.mat');
+PEC_step_jacs = load('MLP_KS_PECstep_tendency_lead1.mat');
+
+
+[v_direct, e_direct] = eig(squeeze(direct_step_jacs.Jacobian_mats(1,:,:)));
+[e_direct, ind_d] = sort(diag(e_direct));
+
+
+[v_Euler, e_Euler] = eig(squeeze(Euler_step_jacs.Jacobian_mats(1,:,:)));
+[e_Euler, ind_e] = sort(diag(e_Euler));
+
+
+
+[v_PEC, e_PEC] = eig(squeeze(PEC_step_jacs.Jacobian_mats(1,:,:)));
+[e_PEC, ind_p] = sort(diag(e_PEC));
+
+theta = linspace(-pi,pi,100);
+x=cos(theta)+1*1i*sin(theta);
+
+figure(1)
+clf
+set(0, 'DefaultAxesFontSize', 20)
+plot(x,'r','Linewidth',2);
+hold on;
+
+plot(e_Euler,'ks', 'MarkerSize',10, 'MarkerFaceColor','k');
+plot(e_direct,'co','MarkerSize',10,'MarkerFaceColor','c');
+plot(e_PEC,'ro','MarkerSize',10,'MarkerFaceColor','r');
+
+legend('Unit Circle','Euler','Direct','RK4',fontsize=10)
+xlabel('$Re(\lambda)$','Interpreter','latex')
+ylabel('$Im(\lambda)$','Interpreter','latex')
+
+figure(2)
+clf
+h = histogram(real(e_direct),50);
+p = histcounts(real(e_direct),50,'Normalization','pdf');
+binCenters = h.BinEdges + (h.BinWidth/2);
+plot(binCenters(1:end-1), p)
+legend('Direct Eigvals',fontsize=10)
+
+figure(3)
+h = histogram(real(e_Euler),50);
+p = histcounts(real(e_Euler),50,'Normalization','pdf');
+binCenters = h.BinEdges + (h.BinWidth/2);
+plot(binCenters(1:end-1), p)
+legend('Euler Eigvals',fontsize=10)
+
+figure(4)
+h = histogram(real(e_PEC),50);
+p = histcounts(real(e_PEC),50,'Normalization','pdf');
+binCenters = h.BinEdges + (h.BinWidth/2);
+plot(binCenters(1:end-1), p)
+legend('PEC Eigvals',fontsize=10)
