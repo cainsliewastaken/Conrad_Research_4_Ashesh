0e2faf7c5bd951af265067a0ecc29857d5d4245b
cainsliewastaken <cainslie@ucsc.edu>
2023-10-06 15:55:36 -0700

    Made single eval file

--- eval_MLP_net.py
+++ eval_MLP_net.py
@@ -24,6 +24,7 @@ input_size = 1024
 hidden_layer_size = 2000
 output_size = 1024
 
+skip_factor = 100 #Number of timesteps to skip (to make the saved data smaller), set to zero to not save a skipped version
 
 path_outputs = '/media/volume/sdb/conrad_stability/model_eval_tendency/' #this is where the saved graphs and .mat files end up
 
@@ -31,7 +32,7 @@ net_file_name = "NN_Spectral_Loss_with_tendencyfft_lambda_reg5_directstep_lead1.
 
 step_func = directstep #this determines the step funciton used in the eval step, has inputs net (pytorch network), input batch, time_step
 
-eval_output_name = 'predicted_directstep_1024_lead'+str(lead)+'_lambda_reg5_tendency.mat'  # what to name the output file
+eval_output_name = 'predicted_directstep_1024_lead'+str(lead)+'_lambda_reg5_tendency'  # what to name the output file
 
 with open('/media/volume/sdb/conrad_stability/training_data/KS_1024.pkl', 'rb') as f: #change for eval data location.
     data = pickle.load(f)
@@ -81,7 +82,14 @@ u_pred_diff_t_n2 = np.diff(net_pred, n=2, axis=0)
 
 # calculate fourier spectrum of time derivative along a single timestep
 u_truth_difft_n2_fspec = np.zeros(np.shape(u_truth_difft_n2[:,:]), dtype=complex)
-u_direct_difft_n2_fspec = np.zeros(np.shape(u_pred_diff_t_n2[:,:]), dtype=complex)
+u_pred_difft_n2_fspec = np.zeros(np.shape(u_pred_diff_t_n2[:,:]), dtype=complex)
+
+
+for n in range(np.shape(u_truth_difft_n2)[0]):
+    u_truth_difft_n2_fspec[n,:] = np.abs(np.fft.fft(u_truth_difft_n2[n,:])) 
+    u_pred_difft_n2_fspec[n,:] = np.abs(np.fft.fft(u_pred_diff_t_n2[n,:])) 
+
+
 
 
 
@@ -90,5 +98,17 @@ matfiledata_output[u'prediction'] = net_pred
 matfiledata_output[u'Truth'] = label_test 
 matfiledata_output[u'RMSE'] = RMSE(net_pred, label_test)
 matfiledata_output[u'Truth_FFT'] = u_1d_fspec_tdim
-matfiledata_output[u'pred_FFT'] = u_direct_difft_n2_fspec
-scipy.io.savemat(path_outputs+eval_output_name, matfiledata_output)
+matfiledata_output[u'pred_FFT'] = u_pred_difft_n2_fspec
+scipy.io.savemat(path_outputs+eval_output_name+'.mat', matfiledata_output)
+
+
+
+if skip_factor: #check if not = 0
+    matfiledata_output_skip = {}
+    matfiledata_output_skip[u'prediction'] = net_pred[0::skip_factor,:]
+    matfiledata_output_skip[u'Truth'] = label_test[0::skip_factor,:]
+    matfiledata_output_skip[u'RMSE'] = RMSE(net_pred, label_test)[0::skip_factor,:]
+    matfiledata_output_skip[u'Truth_FFT'] = u_1d_fspec_tdim[0::skip_factor,:]
+    matfiledata_output_skip[u'pred_FFT'] = u_pred_difft_n2_fspec[0::skip_factor,:]
+
+    scipy.io.savemat(path_outputs+eval_output_name+'_skip'+str(skip_factor)+'.mat', matfiledata_output_skip)
