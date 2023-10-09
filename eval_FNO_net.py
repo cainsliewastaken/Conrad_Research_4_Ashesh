eb851a34fbbc32bd1948c3ad360d488bc3c7c0b8
cainsliewastaken <cainslie@ucsc.edu>
2023-10-09 13:00:29 -0700

    FIle path

--- eval_FNO_net.py
+++ eval_FNO_net.py
@@ -21,7 +21,7 @@ skip_factor = 100 #Number of timesteps to skip (to make the saved data smaller),
 
 path_outputs = '/media/volume/sdb/conrad_stability/model_eval_FNO_tendency/' #this is where the saved graphs and .mat files end up
 
-net_file_name = "NN_Spectral_Loss_FNO_PECstep_tendency_lambda_reg5lead1.pt" #change this to use a different network
+net_file_name = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Spectral_Loss_FNO_PECstep_tendency_lambda_reg5lead1.pt" #change this to use a different network
 
 step_func = PECstep #this determines the step funciton used in the eval step, has inputs net (pytorch network), input batch, time_step
 
