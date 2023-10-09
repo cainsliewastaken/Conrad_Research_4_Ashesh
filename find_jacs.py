893eb25740ff0875854a569745643defd558cf65
cainsliewastaken <cainslie@ucsc.edu>
2023-10-09 13:20:24 -0700

    Untraied model direcstep MLP

--- find_jacs.py
+++ find_jacs.py
@@ -105,7 +105,7 @@ def PECstep(input_batch):
 
 mynet = MLP_Net(input_size, hidden_layer_size, output_size)
 # mynet = FNO1d(modes, width, time_future, time_history)
-mynet.load_state_dict(torch.load("/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Spectral_Loss_with_tendencyfft_lambda_reg5_directstep_lead1.pt"))
+# mynet.load_state_dict(torch.load("/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Spectral_Loss_with_tendencyfft_lambda_reg5_directstep_lead1.pt"))
 mynet.cuda()
 mynet.eval()
 
@@ -113,8 +113,8 @@ ygrad = torch.zeros([eq_points,input_size,input_size])
 
 for k in range(0,eq_points):
 
-    # ygrad [k,:,:] = torch.autograd.functional.jacobian(directstep,x_torch[k,:])
-    ygrad [k,:,:] = torch.func.jacfwd(directstep)(x_torch[k,:])
+    ygrad [k,:,:] = torch.autograd.functional.jacobian(directstep,x_torch[k,:])
+    # ygrad [k,:,:] = torch.func.jacfwd(directstep)(x_torch[k,:])
 
     # temp_mat = torch.autograd.functional.jacobian(PECstep, torch.reshape(torch.tensor(x_torch[k,:]),(1,input_size,1)))
     # ygrad [k,:,:] = torch.reshape(temp_mat,(1,input_size, input_size))
@@ -131,6 +131,6 @@ print(ygrad.shape)
 
 matfiledata = {}
 matfiledata[u'Jacobian_mats'] = ygrad
-scipy.io.savemat(path_outputs+'MLP_KS_Directstep_tendency_lead'+str(lead)+'_jacs.mat', matfiledata)
+scipy.io.savemat(path_outputs+'MLP_KS_Directstep_tendency_lead'+str(lead)+'_UNTRAINED_jacs.mat', matfiledata)
 
 print('Saved Predictions')
