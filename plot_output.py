5437d1d0c88a19da9d200a2e8283e1e4a7779d51
cainsliewastaken <cainslie@ucsc.edu>
2023-10-11 16:49:55 -0700

    Training data plotting

--- plot_output.py
+++ plot_output.py
@@ -3,16 +3,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from canonicalPDEs import load_sol
-u_truth_long = load_sol('KS_1024')
+import pickle
+#%%
 
+with open('/media/volume/sdb/conrad_stability/training_data/KS_1024.pkl', 'rb') as f: #change for eval data location.
+    data = pickle.load(f)
+u_truth_long=np.asarray(data[:,:250000])
 
 #%%
 skip_factor = 100 # only plot the nth timestep, determined by value of skip_factor. Saves memory
+fig = plt.figure()
 plt.contourf(u_truth_long[:,::skip_factor])
 temp, temp, y_min, y_max = plt.axis()
 yrange = [-50, 0, 50]
 plt.yticks([y_min, y_max/2, y_max] , yrange);
 plt.xticks(ticks=plt.xticks()[0][1:], labels=1e-3 * skip_factor * np.array(plt.xticks()[0][1:], dtype=np.float64));
+fig.savefig('Training_data_plot.png')
 
 #%%
 u_truth_long = u_truth_long[:,0:250000]
