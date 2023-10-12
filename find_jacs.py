3d4f6fac2b9ca11868c5df10fd2544e140807a83
cainsliewastaken <cainslie@ucsc.edu>
2023-10-12 11:30:43 -0700

    File Paths

--- find_jacs.py
+++ find_jacs.py
@@ -26,9 +26,9 @@ from torch.profiler import profile, record_function, ProfilerActivity
 lead = 1
 path_outputs = '/media/volume/sdb/conrad_stability/jacobian_mats_all_models/'
 
-model_path = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_FNO_Directstep_lead1.pt"
+model_path = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_FNO_PECstep_lead1.pt"
 
-matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_jacs.mat'
+matfile_name = 'FNO_KS_PECstep_lead'+str(lead)+'_jacs.mat'
 
 def RK4step(input_batch):
  output_1 = mynet(input_batch.cuda())
