6537a2b3bd53fa2bed962905af6df25d2ea9f091
cainsliewastaken <cainslie@ucsc.edu>
2023-10-15 15:24:26 -0700

    Training PEC FNO model

--- find_plot_eigs.m
+++ find_plot_eigs.m
@@ -1,8 +1,8 @@
 direct_step_jacs = load('MLP_KS_Directstep_lead1_jacs.mat');
 PEC_step_jacs = load('MLP_KS_PECstep_lead1_jacs.mat');
 % 
-% direct_step_jacs_FNO = load('FNO_KS_Directstep_lead1_jacs.mat');
-% PEC_step_jacs_FNO = load('FNO_KS_PECstep_lead1_jacs.mat');
+direct_step_jacs_FNO = load('FNO_KS_Directstep_lead1_jacs.mat');
+PEC_step_jacs_FNO = load('FNO_KS_PECstep_lead1_jacs.mat');
 % 
 % % 
 % direct_step_jacs = load('MLP_KS_Directstep_tendency_lead1_jacs.mat');
@@ -34,67 +34,67 @@ PEC_step_jacs_untrained = load('Model_output\MLP_KS_PECstep_tendency_lead1_UNTRA
 
 
 
-% 
-% [v_direct_FNO, e_direct_FNO] = eig(squeeze(direct_step_jacs_FNO.Jacobian_mats(2,:,:)));
-% [e_direct_FNO, ind_d_FNO] = sort(diag(e_direct_FNO));
-% 
-% [v_PEC_FNO, e_PEC_FNO] = eig(squeeze(PEC_step_jacs_FNO.Jacobian_mats(1,:,:)));
-% [e_PEC_FNO, ind_p_FNO] = sort(diag(e_PEC_FNO));
+
+[v_direct_FNO, e_direct_FNO] = eig(squeeze(direct_step_jacs_FNO.Jacobian_mats(2,:,:)));
+[e_direct_FNO, ind_d_FNO] = sort(diag(e_direct_FNO));
+
+[v_PEC_FNO, e_PEC_FNO] = eig(squeeze(PEC_step_jacs_FNO.Jacobian_mats(1,:,:)));
+[e_PEC_FNO, ind_p_FNO] = sort(diag(e_PEC_FNO));
 
 
-% theta = linspace(-pi,pi,100);
-% x=cos(theta)+1*1i*sin(theta);
-% 
-% 
-% figure(1)
-% clf
-% set(0, 'DefaultAxesFontSize', 20)
-% plot(x,'r','Linewidth',2);
-% hold on;
-% 
-% plot(e_direct,'co','MarkerSize',10,'MarkerFaceColor','c','DisplayName','Direct MLP');
-% plot(e_PEC,'ro','MarkerSize',10,'MarkerFaceColor','r','DisplayName','PEC MLP');
-% legend()
-% % plot(e_direct_FNO,'co','MarkerSize',10);
-% % plot(e_PEC_FNO,'ro','MarkerSize',10);
-% 
-% % legend('Unit Circle','Direct MLP','PEC MLP','Direct FNO','PEC FNO',fontsize=10)
-% xlabel('$Re(\lambda)$','Interpreter','latex')
-% ylabel('$Im(\lambda)$','Interpreter','latex')
-% 
-% 
-% 
-% figure(12)
-% clf
-% set(0, 'DefaultAxesFontSize', 20)
-% plot(x,'r','Linewidth',2);
-% hold on;
-% 
-% plot(e_direct,'co','MarkerSize',10,'MarkerFaceColor','c');
-% 
+theta = linspace(-pi,pi,100);
+x=cos(theta)+1*1i*sin(theta);
+
+
+figure(1)
+clf
+set(0, 'DefaultAxesFontSize', 20)
+plot(x,'r','Linewidth',2);
+hold on;
+
+plot(e_direct,'co','MarkerSize',10,'MarkerFaceColor','c','DisplayName','Direct MLP');
+plot(e_PEC,'ro','MarkerSize',10,'MarkerFaceColor','r','DisplayName','PEC MLP');
+legend()
 % plot(e_direct_FNO,'co','MarkerSize',10);
-% 
-% legend('Unit Circle','Direct MLP','Direct FNO',fontsize=10)
-% xlabel('$Re(\lambda)$','Interpreter','latex')
-% ylabel('$Im(\lambda)$','Interpreter','latex')
-% 
-% 
-% 
-% 
-% figure(13)
-% clf
-% set(0, 'DefaultAxesFontSize', 20)
-% % plot(x,'r','Linewidth',2);
-% hold on;
-% 
-% plot(e_PEC,'ro','MarkerSize',10,'MarkerFaceColor','r');
-% 
 % plot(e_PEC_FNO,'ro','MarkerSize',10);
-% 
-% legend('Unit Circle','PEC MLP','PEC FNO',fontsize=10)
-% xlabel('$Re(\lambda)$','Interpreter','latex')
-% ylabel('$Im(\lambda)$','Interpreter','latex')
-% 
+
+% legend('Unit Circle','Direct MLP','PEC MLP','Direct FNO','PEC FNO',fontsize=10)
+xlabel('$Re(\lambda)$','Interpreter','latex')
+ylabel('$Im(\lambda)$','Interpreter','latex')
+
+
+
+figure(12)
+clf
+set(0, 'DefaultAxesFontSize', 20)
+plot(x,'r','Linewidth',2);
+hold on;
+
+plot(e_direct,'co','MarkerSize',10,'MarkerFaceColor','c');
+
+plot(e_direct_FNO,'co','MarkerSize',10);
+
+legend('Unit Circle','Direct MLP','Direct FNO',fontsize=10)
+xlabel('$Re(\lambda)$','Interpreter','latex')
+ylabel('$Im(\lambda)$','Interpreter','latex')
+
+
+
+
+figure(13)
+clf
+set(0, 'DefaultAxesFontSize', 20)
+plot(x,'r','Linewidth',2);
+hold on;
+
+plot(e_PEC,'ro','MarkerSize',10,'MarkerFaceColor','r');
+
+plot(e_PEC_FNO,'ro','MarkerSize',10);
+
+legend('Unit Circle','PEC MLP','PEC FNO',fontsize=10)
+xlabel('$Re(\lambda)$','Interpreter','latex')
+ylabel('$Im(\lambda)$','Interpreter','latex')
+
 
 
 
@@ -123,8 +123,8 @@ legend('PEC Eigvals','PEC untrained',fontsize=10)
 
 
 
-
-
+% 
+% 
 % 
 % figure(4)
 % clf
@@ -135,7 +135,7 @@ legend('PEC Eigvals','PEC untrained',fontsize=10)
 % histogram(abs(e_PEC))
 % legend('PEC Eigvals',fontsize=10)
 % 
-
+% 
 % 
 % figure(5)
 % clf
@@ -146,7 +146,7 @@ legend('PEC Eigvals','PEC untrained',fontsize=10)
 % histogram(abs(e_direct_FNO))
 % 
 % legend('Direct Eigvals FNO',fontsize=10)
-
+% 
 % 
 % figure(6)
 % clf
@@ -156,6 +156,6 @@ legend('PEC Eigvals','PEC untrained',fontsize=10)
 % %plot(binCenters(1:end-1), p)
 % histogram(abs(e_PEC_FNO))
 % legend('PEC Eigvals FNO',fontsize=10)
-
+% 
 
 
---
+++ nn_train_FNO.py
@@ -0,0 +1,87 @@
+import numpy as np
+import torch
+print(torch.__version__)
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#from torchinfo import summary
+from count_trainable_params import count_parameters
+import pickle
+from nn_FNO import FNO1d
+from nn_step_methods import Directstep, Eulerstep, RK4step, PECstep, PEC4step
+
+lead=1
+
+path_outputs = '/media/volume/sdb/conrad_stability/model_eval_FNO/'
+
+step_func = PECstep
+
+net_file_name = 'NN_FNO_PECstep_lead'+str(lead)+'.pt'
+
+
+
+
+with open('/media/volume/sdb/conrad_stability/training_data/KS_1024.pkl', 'rb') as f:
+    data = pickle.load(f)
+data=np.asarray(data[:,:250000])
+
+
+lead=1
+time_step = 1e-3
+trainN = 150000
+input_size = 1024
+output_size = 1024
+hidden_layer_size = 2000
+input_train_torch = torch.from_numpy(np.transpose(data[:,0:trainN])).float().cuda()
+label_train_torch = torch.from_numpy(np.transpose(data[:,lead:lead+trainN])).float().cuda()
+
+input_test_torch = torch.from_numpy(np.transpose(data[:,trainN:])).float().cuda()
+label_test_torch = torch.from_numpy(np.transpose(data[:,trainN+lead:])).float().cuda()
+label_test = np.transpose(data[:,trainN+lead:])
+
+
+time_history = 1 #time steps to be considered as input to the solver
+time_future = 1 #time steps to be considered as output of the solver
+device = 'cuda'  #change to cpu if no cuda available
+
+#model parameters
+modes = 256 # number of Fourier modes to multiply
+width = 64 # input and output chasnnels to the FNO layer
+
+learning_rate = 0.0001
+lr_decay = 0.4
+
+
+mynet = FNO1d(modes, width, time_future, time_history).cuda()
+count_parameters(mynet)
+
+optimizer = optim.AdamW(mynet.parameters(), lr=learning_rate)
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 5, 10, 15], gamma=lr_decay)
+
+
+
+loss_fn = nn.MSELoss()
+epochs = 60
+batch_size = 100
+
+for ep in range(0, epochs+1):
+    for step in range(0,trainN,batch_size):
+        indices = np.random.permutation(np.arange(start=step, step=1,stop=step+batch_size))
+        input_batch, label_batch = input_train_torch[indices], label_train_torch[indices]
+        input_batch = torch.reshape(input_batch,(batch_size,input_size,1))
+        label_batch = torch.reshape(label_batch,(batch_size,input_size,1))
+        #pick a random boundary batch
+        optimizer.zero_grad()
+        outputs = step_func(mynet, input_batch, time_step)
+        loss = loss_fn(outputs, label_batch)
+
+        loss.backward(retain_graph=True)
+        optimizer.step()
+
+
+    if ep % 10 == 0:
+        print('Epoch', ep)
+        print ('Loss', loss)
+
+torch.save(mynet.state_dict(), net_file_name)
