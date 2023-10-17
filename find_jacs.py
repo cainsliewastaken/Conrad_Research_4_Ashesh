58bc3753b29cea987373e37042a6532ecf0043cc
cainsliewastaken <cainslie@ucsc.edu>
2023-10-17 16:23:24 -0700

    File name typo

--- find_jacs.py
+++ find_jacs.py
@@ -28,7 +28,7 @@ path_outputs = '/media/volume/sdb/conrad_stability/jacobian_mats_all_models/'
 
 model_path = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_FNO_Directstep_lead1.pt"
 
-matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'+UNTRAINED_jacs.mat'
+matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_UNTRAINED_jacs.mat'
 
 
 print('loading data')
