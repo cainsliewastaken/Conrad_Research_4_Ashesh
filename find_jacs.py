aae047a9b63a5a45d2e8484de1089aa701c706fa
cainsliewastaken <cainslie@ucsc.edu>
2023-10-17 19:36:25 -0700

    typo

--- find_jacs.py
+++ find_jacs.py
@@ -128,7 +128,7 @@ with profile(activities=[ProfilerActivity.CUDA],
         profile_memory=True) as prof:
     mynet(torch.reshape(input_test_torch[0,:],(1,input_size,1)))
 
-print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
+print(prof.key_averages().table(sort_by="self_gpu_memory_usage", row_limit=10))
 
 
 for k in range(0,eq_points):
