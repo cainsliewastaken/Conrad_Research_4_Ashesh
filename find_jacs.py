21793e838627476750a20a0ceef493dfba290fdf
cainsliewastaken <cainslie@ucsc.edu>
2023-10-24 14:44:55 -0700

    Stack trace

--- find_jacs.py
+++ find_jacs.py
@@ -126,7 +126,7 @@ with profile(activities=[ProfilerActivity.CPU ,ProfilerActivity.CUDA],  with_sta
   mynet(torch.reshape(input_test_torch[0,:].cuda(),(1,input_size,1)))
 
 # print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cpu_memory_usage", row_limit=10))
-print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cpu_memory_usage", row_limit=10))
+print(prof.key_averages(group_by_stack_n=5).table(sort_by="self_cuda_time_total", row_limit=10))
 
 
 
