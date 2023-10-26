14ebeef265e1eeb2c31466027808e70c0abc656e
cainsliewastaken <cainslie@ucsc.edu>
2023-10-26 14:59:39 -0700

    Double lr

--- nn_train_MLP.py
+++ nn_train_MLP.py
@@ -52,7 +52,7 @@ count_parameters(mynet)
 
 
 #use two optimizers.  learing rates seem to work.
-optimizer = optim.SGD(mynet.parameters(), lr=0.005)
+optimizer = optim.SGD(mynet.parameters(), lr=0.01)
 
 loss_fn = nn.MSELoss()
 epochs = 100
