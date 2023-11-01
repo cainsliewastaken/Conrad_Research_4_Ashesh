54d8d0709afd1c65a7a5a09d3c1f0072fcfe091b
cainsliewastaken <cainslie@ucsc.edu>
2023-11-01 01:27:24 -0700

    Make sure to use directstep

--- find_jacs.py
+++ find_jacs.py
@@ -106,9 +106,9 @@ def Directstep(input_batch):
   output_1 = mynet(input_batch.cuda())
   return output_1
 
-def PECstep(input_batch):
- output_1 = mynet(input_batch.cuda()) + input_batch.cuda()
- return input_batch.cuda() + time_step*0.5*(mynet(input_batch.cuda())+mynet(output_1))
+# def PECstep(input_batch):
+#  output_1 = mynet(input_batch.cuda()) + input_batch.cuda()
+#  return input_batch.cuda() + time_step*0.5*(mynet(input_batch.cuda())+mynet(output_1))
 
 # print(torch.cuda.memory_allocated())
 step_func = Directstep
