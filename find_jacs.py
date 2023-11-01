3d8cda9c5d5c505f0947e8ef40238896f2ebae59
cainsliewastaken <cainslie@ucsc.edu>
2023-11-01 01:28:35 -0700

    Removing changes

--- find_jacs.py
+++ find_jacs.py
@@ -29,7 +29,7 @@ path_outputs = '/media/volume/sdb/conrad_stability/jacobian_mats_all_models/'
 
 model_path = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_FNO_Directstep_lead1.pt"
 
-matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_NEW_jacs.mat'
+matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_jacs.mat'
 
 
 print('loading data')
