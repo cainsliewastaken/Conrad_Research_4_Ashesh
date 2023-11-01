d3068675dad5da2929c0629bf20f2988cd718385
cainsliewastaken <cainslie@ucsc.edu>
2023-10-31 22:19:18 -0700

    Directstep FNO untrained

--- find_jacs.py
+++ find_jacs.py
@@ -29,7 +29,7 @@ path_outputs = '/media/volume/sdb/conrad_stability/jacobian_mats_all_models/'
 
 model_path = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_FNO_Directstep_lead1_tendency.pt"
 
-matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_tendency_jacs.mat'
+matfile_name = 'FNO_KS_Directstep_lead'+str(lead)+'_UNTRAINED_jacs.mat'
 
 
 print('loading data')
@@ -81,9 +81,9 @@ for k in (np.array([ int(0),  int(10000), int(20000), int(99999)])):
 
 # mynet = MLP_Net(input_size, hidden_layer_size, output_size)
 mynet = FNO1d(modes, width, time_future, time_history)
-mynet.load_state_dict(torch.load(model_path))
-print('model defined')
-print(torch.cuda.memory_allocated())
+# mynet.load_state_dict(torch.load(model_path))
+# print('model defined')
+# print(torch.cuda.memory_allocated())
 mynet.cuda()
 print('model cuda')
 print(torch.cuda.memory_allocated())
