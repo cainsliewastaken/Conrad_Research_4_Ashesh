2da161a00c9677e9b57f8aaa7ff76c046e242b1a
cainsliewastaken <cainslie@ucsc.edu>
2023-11-24 16:02:07 -0800

    cuda testing

--- nn_train_FNO.py
+++ nn_train_FNO.py
@@ -16,7 +16,7 @@ lead=1
 
 path_outputs = '/media/volume/sdb/conrad_stability/model_eval_FNO_tendency/'
 
-step_func = Directstep.cuda()
+step_func = Directstep
 
 net_file_name = 'NN_FNO_Directstep_lead'+str(lead)+'_tendency_mode256.pt'
 
@@ -57,8 +57,8 @@ lr_decay = 0.4
 mynet = FNO1d(modes, width, time_future, time_history).cuda()
 count_parameters(mynet)
 
-optimizer = optim.AdamW(mynet.parameters(), lr=learning_rate)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 5, 10, 15], gamma=lr_decay)
+optimizer = optim.AdamW(mynet.parameters(), lr=learning_rate).cuda()
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[0, 5, 10, 15], gamma=lr_decay).cuda()
 
 
 
