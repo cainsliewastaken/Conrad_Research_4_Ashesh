2baf9e1a282bd787c51cc1da452b53aa5fcc46b7
cainsliewastaken <cainslie@ucsc.edu>
2023-11-24 16:06:40 -0800

    cuda testing

--- nn_train_FNO.py
+++ nn_train_FNO.py
@@ -74,7 +74,7 @@ torch.set_printoptions(precision=10)
 for ep in range(0, epochs+1):
     for step in range(0,trainN,batch_size):
         indices = np.random.permutation(np.arange(start=step, step=1,stop=step+batch_size))
-        input_batch, label_batch, du_label_batch = input_train_torch[indices], label_train_torch[indices], du_label_torch[indices]
+        input_batch, label_batch, du_label_batch = input_train_torch[indices].cuda(), label_train_torch[indices].cuda(), du_label_torch[indices].cuda()
         input_batch = torch.reshape(input_batch,(batch_size,input_size,1)).float()
         label_batch = torch.reshape(label_batch,(batch_size,input_size,1)).float()
         du_label_batch = torch.reshape(du_label_batch,(batch_size,input_size,1)).float()
