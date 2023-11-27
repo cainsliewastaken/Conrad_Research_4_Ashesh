6f944629b2aad91425e141f698671e14329e947e
cainsliewastaken <cainslie@ucsc.edu>
2023-11-26 20:10:56 -0800

    eval new direct mlp

--- eval_MLP_net.py
+++ eval_MLP_net.py
@@ -30,11 +30,11 @@ skip_factor = 100 #Number of timesteps to skip (to make the saved data smaller),
 
 path_outputs = '/media/volume/sdb/conrad_stability/model_eval_tendency/' #this is where the saved graphs and .mat files end up
 
-net_file_name = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Directstep_lead1_tendency_new.pt" #change this to use a different network
+net_file_name = "/home/exouser/conrad_net_stability/Conrad_Research_4_Ashesh/NN_Directstep_lead1_tendency.pt" #change this to use a different network
 
 step_func = Directstep #this determines the step funciton used in the eval step, has inputs net (pytorch network), input batch, time_step
 
-eval_output_name = 'predicted_Directstep_1024_tendency_lead'+str(lead)+'_new'  # what to name the output file, .mat ending not needed
+eval_output_name = 'predicted_Directstep_1024_tendency_lead'+str(lead)+''  # what to name the output file, .mat ending not needed
 
 with open('/media/volume/sdb/conrad_stability/training_data/KS_1024.pkl', 'rb') as f: #change for eval data location.
     data = pickle.load(f)
