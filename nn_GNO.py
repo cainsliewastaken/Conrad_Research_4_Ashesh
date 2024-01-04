11ae219533421142f838b7e407e59a9ff6686498
cainsliewastaken <cainslie@ucsc.edu>
2024-01-03 18:35:45 -0800

    Added GNO

---
+++ nn_GNO.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+print(torch.__version__)
+import torch.nn as nn
+import torch.nn.functional as F
+from graph_pde.graph_neural_operator.UAI1_full_resolution import KernelNN
+
+
