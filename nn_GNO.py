4fcc5ed2a81b12fcec97098dcde1345165493e6b
cainsliewastaken <cainslie@ucsc.edu>
2024-01-10 19:20:44 -0800

    debugging

--- nn_GNO.py
+++ nn_GNO.py
@@ -50,6 +50,7 @@ class KernelNN(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
+        print(len(x))
         x = self.fc1(x)
         for k in range(self.depth):
             x = F.relu(self.conv1(x, edge_index, edge_attr))
@@ -297,8 +298,7 @@ for ep in range(0, epochs+1):
             graph_single = torch_geometric.data.Data(x = input_batch[j,:].cuda(), 
                     y = label_batch[j,:], 
                     edge_index = edge_index, 
-                    edge_attr = edge_attr, 
-                    )
+                    edge_attr = edge_attr, )
             output = step_func(mynet, graph_single, time_step)
             loss += loss_func(output, label_batch[j,:])  # use this loss function for mse loss
 
