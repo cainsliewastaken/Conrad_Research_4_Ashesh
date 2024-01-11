d035aaf42a387b5523d71fe5aed450f2374ce041
cainsliewastaken <cainslie@ucsc.edu>
2024-01-10 16:41:50 -0800

    bug fixing

--- nn_GNO.py
+++ nn_GNO.py
@@ -295,9 +295,9 @@ for ep in range(0, epochs+1):
             edge_attr = meshgenerator.attributes(theta = input_batch[j,:])
             graph_batch.append(torch_geometric.data.Data(x = input_batch[j,:].cuda(), y = label_batch[j,:], edge_index = edge_index, edge_attr = edge_attr))
         label_batch.cuda(), du_label_batch.cuda()
-
+        print(graph_batch)
         optimizer.zero_grad()
-        outputs = step_func(mynet, graph_batch[:], time_step)
+        outputs = step_func(mynet, graph_batch, time_step)
         
         loss = loss_func(outputs, label_batch)  # use this loss function for mse loss
 
