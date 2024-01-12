1153e65f7b39988c3ad76b3518ac931eef461a1b
cainsliewastaken <cainslie@ucsc.edu>
2024-01-11 17:43:57 -0800

    print j

--- nn_GNO.py
+++ nn_GNO.py
@@ -268,7 +268,7 @@ for ep in range(0, epochs+1):
         loss = 0
         for j in range(batch_size):
             output = step_func(mynet, input_batch[j,:].float(), time_step)
-            print(j)
+            print(j,"j")
             loss += loss_func(output, label_batch[j,:].float())  # use this loss function for mse loss
         
             # output_2 = step_func(mynet, output, time_step) #use these two lines for spectral loss in tendency
