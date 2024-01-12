6cbb90e8e1ba50acb182bf92939be449a8e1b04f
cainsliewastaken <cainslie@ucsc.edu>
2024-01-11 16:34:48 -0800

    smaller batches, 1 epoch

--- nn_GNO.py
+++ nn_GNO.py
@@ -274,8 +274,8 @@ scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step,
 
 
 
-epochs = 60
-batch_size = 10
+epochs = 1
+batch_size = 5
 wavenum_init = 100
 lamda_reg = 5
 
