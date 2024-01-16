73418c3614831fdb2ed05890e283291288d1b7d0
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 13:44:57 -0800

    FNO PEC

--- Make_RMSE_solo_file.py
+++ Make_RMSE_solo_file.py
@@ -2,11 +2,11 @@ import scipy.io
 
 
 path_outputs = '/media/volume/sdb/conrad_stability/RMSE_data/'
-eval_output_name = 'FNO_predicted_'+'Directstep'+'_RMSE'
+eval_output_name = 'FNO_predicted_'+'PECstep'+'_RMSE'
 
 
 mat_file = {}
-scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_FNO/predicted_Directstep_1024_FNO_lead1.mat', mat_file)
+scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_FNO/predicted_PECstep_1024_FNO_lead1.mat', mat_file)
 data = {}
 data[u'RMSE'] = mat_file['RMSE']
 scipy.io.savemat(path_outputs+eval_output_name+'.mat', data)
