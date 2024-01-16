e6ea79c35c5b898df147f9b63a5d9c9b2c1d2660
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 14:14:49 -0800

    Updated

--- make_RMSE_plot.m
+++ make_RMSE_plot.m
@@ -1,12 +1,12 @@
-model_one = load('MLP_predicted_Directstep_1024_lead1.mat');
-model_two = load('MLP_predicted_PECstep_1024_lead1.mat');
+model_one = load('MLP_predicted_Directstep_RMSE.mat');
+model_two = load('MLP_predicted_PECstep_RMSE.mat');
 
-model_four = load('MLP_predicted_PECstep_1024_tendency_lead1.mat');
+model_four = load('MLP_predicted_PECstep_tendnecy_RMSE.mat');
 
-% model_one = load('FNO_predicted_Directstep_1024_lead1.mat');
-% model_two = load('FNO_predicted_PECstep_1024_lead1.mat');
+% model_one = load('FNO_predicted_Directstep_RMSE.mat');
+% model_two = load('FNO_predicted_PECstep_RMSE.mat');
 % 
-% model_four = load('FNO_predicted_PECstep_1024_lead1_tendency.mat');
+% model_four = load('FNO_predicted_PECstep_tendency_RMSE.mat');
 
 figure(3)
 clf
@@ -16,5 +16,3 @@ plot(model_two.RMSE,'DisplayName','PEC Step');
 plot(model_four.RMSE,'DisplayName','PEC Step spectral loss')
 legend(Location='northwest')
 axis([1 300 -.5 5])
-
-saveas(figure(3), 'RMSE_MLP')
