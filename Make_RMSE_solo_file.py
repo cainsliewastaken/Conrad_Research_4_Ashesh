459910ff1b5a038f8eb61aa88e1da5dbaf122156
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 13:42:09 -0800

    PEC tendency MLP

--- Make_RMSE_solo_file.py
+++ Make_RMSE_solo_file.py
@@ -2,11 +2,11 @@ import scipy.io
 
 
 path_outputs = '/media/volume/sdb/conrad_stability/RMSE_data/'
-eval_output_name = 'MLP_predicted_'+'PECstep'+'_RMSE'
+eval_output_name = 'MLP_predicted_'+'PECstep'+'_tendnecy_RMSE'
 
 
 mat_file = {}
-scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval/predicted_PECstep_1024_lead1.mat', mat_file)
+scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_tendency/predicted_PECstep_1024_tendency_lead1.mat', mat_file)
 data = {}
 data[u'RMSE'] = mat_file['RMSE']
 scipy.io.savemat(path_outputs+eval_output_name+'.mat', data)
