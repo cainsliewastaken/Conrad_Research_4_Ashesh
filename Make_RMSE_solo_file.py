1c916a588acfd62aa0c8cf7d1055231f339fc65f
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 13:54:20 -0800

    FNO PEC tendency

--- Make_RMSE_solo_file.py
+++ Make_RMSE_solo_file.py
@@ -6,7 +6,7 @@ eval_output_name = 'FNO_predicted_'+'PECstep'+'_tendency_RMSE'
 
 
 mat_file = {}
-scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_FNO_tendency/predicted_PECstep_1024_FNO_lead1.mat', mat_file)
+scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_FNO_tendency/predicted_PECstep_1024_FNO_lead1_tendency.mat', mat_file)
 data = {}
 data[u'RMSE'] = mat_file['RMSE']
 scipy.io.savemat(path_outputs+eval_output_name+'.mat', data)
