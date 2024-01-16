0489675d2a60693db4ed1a07150dd5298cb56c1a
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 13:34:27 -0800

    Change

--- Make_RMSE_solo_file.py
+++ Make_RMSE_solo_file.py
@@ -8,4 +8,5 @@ eval_output_name = 'predicted_'+'Directstep'+'_RMSE'
 mat_file = {}
 scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval/predicted_Directstep_1024_lead1.mat', mat_file)
 print(mat_file.keys())
-scipy.io.savemat(path_outputs+eval_output_name+'.mat', mat_file[u'RMSE'])
+data = mat_file['RMSE']
+scipy.io.savemat(path_outputs+eval_output_name+'.mat', data)
