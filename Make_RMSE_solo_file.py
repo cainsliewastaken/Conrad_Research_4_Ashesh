436146e10a9ab4aa1f870850b4a467140344adcb
cainsliewastaken <cainslie@ucsc.edu>
2024-01-16 22:15:57 -0800

    file path bug

--- Make_RMSE_solo_file.py
+++ Make_RMSE_solo_file.py
@@ -6,7 +6,7 @@ eval_output_name = 'MLP_predicted_'+'PECstep'+'_tendency_RMSE'
 
 
 mat_file = {}
-scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval/predicted_PECstep_1024_lead1_tendency.mat', mat_file)
+scipy.io.loadmat('/media/volume/sdb/conrad_stability/model_eval_tendency/predicted_PECstep_1024_lead1_tendency.mat', mat_file)
 data = {}
 data[u'RMSE'] = mat_file['RMSE']
 scipy.io.savemat(path_outputs+eval_output_name+'.mat', data)
